d381aa8201cc29dcae1e37bef39ee2f819bf06ae
Antoine Beyeler <abeyeler@ab-ware.com>
2022-03-31 15:19:58 +0200

    Fixed wobbly cylinder example

--- examples/wobbly_cylinders/sketch_wobbly_cylinders.py
+++ examples/wobbly_cylinders/sketch_wobbly_cylinders.py
@@ -12,7 +12,7 @@ class WobblyCylindersSketch(vsketch.SketchClass):
                 vsk.circle(j, 5, 5 * vsk.noise(i * 0.15, 0, j))
                 vsk.circle(j, 15, 5 * vsk.noise(i * 0.15, 1, j))
 
-        vsk.vpype("ptranslate 0 0 3cm perspective -f 120 -dz 0.5cm occult -i color blue")
+        vsk.vpype("pspread 0.5cm ptranslate 0 0 3cm perspective -f 120 occult -i color blue")
         vsk.vpype("crop 5mm 5mm '%prop.vp_page_size[0]-1*cm%' '%prop.vp_page_size[1]-1*cm%'")
         vsk.vpype("rect 5mm 5mm '%prop.vp_page_size[0]-1*cm%' '%prop.vp_page_size[1]-1*cm%'")
         vsk.vpype("lmove all 1")
