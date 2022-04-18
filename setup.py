2eafedafdbc4de7f88f2ae0023f7f1383aeaab0a
Antoine Beyeler <abeyeler@ab-ware.com>
2022-04-18 15:42:28 +0200

    Bumped version to 0.2.0a0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setup(
     name="vpype-perspective",
-    version="0.1.0",
+    version="0.2.0a0",
     description="Put your art in perspective with vpype.",
     long_description=readme,
     long_description_content_type="text/markdown",
